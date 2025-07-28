deecf8258866af9b25a729203b8163f839af2dc9
Juliabrasil523 <julia.brasil19@escola.pr.gov.br>
2025-07-28 12:06:41 -0300

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 function criaCartao(categoria, pergunta, resposta) {
         let container = document.getElementById('container')
@@ -25,4 +25,3 @@ function criaCartao(categoria, pergunta, resposta) {
 cartao.addEventListener('click', viraCartao)
 container.appendChild(cartao)
 }
->>>>>>> d02fd8a5481a343ff922a130a8d0746036d7a4a0
