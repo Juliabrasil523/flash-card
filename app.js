43211e4798777b567deda80942adb07cbc16b67e
Juliabrasil523 <julia.brasil19@escola.pr.gov.br>
2025-07-28 12:04:54 -0300

    index

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+<<<<<<< HEAD
+
 function criaCartao(categoria, pergunta, resposta) {
         let container = document.getElementById('container')
         let cartao = document.createElement('article')
         cartao.className = 'cartao'
+
+        cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+       let respostaEstaVisivel = false
+
+    function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+container.appendChild(cartao)
+}
+>>>>>>> d02fd8a5481a343ff922a130a8d0746036d7a4a0
